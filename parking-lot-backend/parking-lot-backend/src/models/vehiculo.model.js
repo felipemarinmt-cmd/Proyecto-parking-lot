3763313d53bea3d52f53648f2811d29209ab5d95
felipemarinmt-cdm <felipemarinmt@gmail.com>
2026-02-26 13:39:06 -0500

    vehiculosModelJs

---
+++ parking-lot-backend/parking-lot-backend/src/models/vehiculo.model.js
@@ -0,0 +1,36 @@
+const db = require("../config/db");
+const { findByDocumento } = require("./usuario.model");
+const vehiculo = {
+  getAll: async () => {
+    const [rows] = await db.query("SELECT * FROM VEHICULO");
+    return rows;
+  },
+
+  findByDocumento: async (placa) => {
+    const [rows] = await db.query("SELECT * FROM VEHICULO WHERE placa = ?", [
+      placa,
+    ]);
+    return rows[0];
+  },
+
+  create: async (vehiculoData) => {
+    const { placa, usuario_id } = vehiculoData;
+    const [result] = await db.query(
+      "INSERT INTO VEHICULO (placa, usuario_id) VALUES (?, ?)",
+      [placa, usuario_id],
+    );
+    return result;
+  },
+
+  update: async (id, data) => {
+    const { placa } = data;
+    await db.query("UPDATE VEHICULO SET placa = ? WHERE id_vehiculo = ?", [
+      placa,
+      id,
+    ]);
+  },
+
+  delete: async (id) => {
+    await db.query("DELETE FROM VEHICULO WHERE id_vehiculo = ?", [id]);
+  },
+};
